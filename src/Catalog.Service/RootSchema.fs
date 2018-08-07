4c85f9fa6139e8132b449cd937516435ecaac9ee
Rajiv Mounguengue <rajiv.mounguengue@hotmail.fr>
2018-08-07 18:06:21 +0100

    Feature/product brand mutations (#4)
    * Added solution file.
    
    * Added create product brand mutation.

--- src/Catalog.Service/RootSchema.fs
+++ src/Catalog.Service/RootSchema.fs
@@ -2,19 +2,15 @@ namespace Catalog.API
 
 open FSharp.Data.GraphQL
 open FSharp.Data.GraphQL.Types
-open FSharp.Data.GraphQL.Server.Middlewares
 open Catalog.API.ProductBrandSchema
 open Catalog.API.ProductCategorySchema
-open System.ComponentModel
 
 type Root =
     { ClientId: System.Guid }
 
 module Schema =
 
-    //let SchemaConfig = SchemaConfig.Default
-
-    let brands = [
+    let mutable brands = [
                    {Id = "681f3f83-2580-4c54-ac0a-f18dd1b0d73a" |> System.Guid; Name = "Apple"}
                    {Id = "cc6b592e-b344-4daa-85d9-85ff501dc59c" |> System.Guid; Name = "Nokia"} 
                    {Id = "c79fdfc5-cfa8-43ac-8617-9df4b94c4cd1" |> System.Guid; Name = "Samsung"}
@@ -81,5 +77,29 @@ module Schema =
                                })
                 ])
 
-    let executor = Schema(query = Query) :> ISchema<Root> |> Executor
+    let Mutation =
+        Define.Object<Root>(
+            name = "Mutation",
+            fields = [
+                Define.AsyncField(
+                            "createProductBrand",
+                            ID,
+                            "Create a new product brand",
+                            [ Define.Input("input", CreateProductBrandInput) ],
+                            fun ctx _ -> async {
+                                let input = ctx.Arg<CreateProductBrandDto>("input")
+
+                                let id = System.Guid.NewGuid()
+
+                                match brands |> List.exists (fun brand -> brand.Name.ToLower() = input.Name.ToLower()) with
+                                | false ->
+                                    brands <- brands |> List.append [{Id = id; Name = input.Name}]
+                                | true -> 
+                                    failwith (sprintf "A product brand with name %s alrady exists" input.Name)
+                                
+                                return id
+                              })
+            ])
+
+    let executor = Schema(query = Query, mutation = Mutation) :> ISchema<Root> |> Executor
     let root = { ClientId = System.Guid.NewGuid() }

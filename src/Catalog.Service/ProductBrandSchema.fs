7106835cc29348ec41a7dd13b28839c1f5f8b615
Rajiv Mounguengue <rajiv.mounguengue@hotmail.fr>
2018-08-07 17:59:56 +0100

    Added create product brand mutation.

--- src/Catalog.Service/ProductBrandSchema.fs
+++ src/Catalog.Service/ProductBrandSchema.fs
@@ -1,17 +1,22 @@
-namespace Catalog.API
+module Catalog.API.ProductBrandSchema
 
 open FSharp.Data.GraphQL.Types
 
-type ProductBrandDto = {Id: System.Guid; Name: System.String}
+type ProductBrandDto = {Id: System.Guid; Name: string}
 
-module ProductBrandSchema =
-    let rec ProductBrandType =
-        Define.Object<ProductBrandDto>(
-                name = "ProductBrand",
-                description = "A product brand.",
-                isTypeOf = (fun o -> o :? ProductBrandDto),
-                fieldsFn = fun () ->
-                [
-                    Define.Field("id", ID<System.Guid>, "The id of the product brand.", fun _ dto -> dto.Id)
-                    Define.Field("name", String, "The name of the product brand.", fun _ dto -> dto.Name)
-                ])
+type CreateProductBrandDto = { Name: string }
+
+let CreateProductBrandInput = 
+        Define.InputObject<CreateProductBrandDto>("CreateProductBrandInput", 
+            [ Define.Input("name", String) ])
+
+let rec ProductBrandType =
+    Define.Object<ProductBrandDto>(
+            name = "ProductBrand",
+            description = "A product brand.",
+            isTypeOf = (fun o -> o :? ProductBrandDto),
+            fieldsFn = fun () ->
+            [
+                Define.Field("id", ID<System.Guid>, "The id of the product brand.", fun _ dto -> dto.Id)
+                Define.Field("name", String, "The name of the product brand.", fun _ dto -> dto.Name)
+            ])

f84d2da1f8b79fddc7d4454d42543137569e66fe
Rajiv Mounguengue <rajiv.mounguengue@hotmail.fr>
2018-09-02 18:21:57 +0100

    Added more tests.

--- src/Catalog.Domain/ProductBrands/ProductBrand.Types.fs
+++ src/Catalog.Domain/ProductBrands/ProductBrand.Types.fs
@@ -6,9 +6,20 @@ type ProductBrandId = private ProductBrandId of Guid
 
 module ProductBrandId =
     let create value = match value = Guid.Empty with
-                        | true -> "Product brand id can not be empty" |> Error
-                        | false -> value |> ProductBrandId |> Ok
+                       | true -> "Product brand id can not be empty" |> Error
+                       | false -> value |> ProductBrandId |> Ok
 
     let getValue (ProductBrandId value) = value
 
     let Empty = Guid.Empty |> ProductBrandId
+
+type ProductBrandName= private ProductBrandName of string
+
+module ProductBrandName =
+    let create value = match value |> String.IsNullOrWhiteSpace with
+                       | true -> "Product brand id can not be empty" |> Error
+                       | false -> value |> ProductBrandName |> Ok
+
+    let getValue (ProductBrandName value) = value
+
+    let Empty = String.Empty |> ProductBrandName
--- tests/Catalog.Domain.Tests/ProductBrands/ProductBrand.Tests.fs
+++ tests/Catalog.Domain.Tests/ProductBrands/ProductBrand.Tests.fs
@@ -6,31 +6,120 @@ open Catalog.Domain
 
 [<Tests>]
 let tests =
-  testList "ProductBrandId" [
-    testList "Create" [
-        test "Should return error on empty value" {
+    testList "ProductBrand" [
+        testList "ProductBrandId" [
+            testList "Create" [
+                test "Should return error on empty value" {
 
-          // Arrange
-          let value = Guid.Empty
+                  // Arrange
+                  let value = Guid.Empty
 
-          // Act
-          let result = value |> ProductBrandId.create
+                  // Act
+                  let result = value |> ProductBrandId.create
 
-          // Assert
-          Expect.isError result "Value is empty"
-        }
+                  // Assert
+                  Expect.isError result "Value is empty"
+                }
 
-        test "Should return ok on correct value" {
+                test "Should return ok on correct value" {
 
-          // Arrange
-          let value = Guid.NewGuid()
+                  // Arrange
+                  let value = Guid.NewGuid()
 
-          // Act
-          let result = value |> ProductBrandId.create
+                  // Act
+                  let result = value |> ProductBrandId.create
 
-          // Assert
-          Expect.isOk result "Value is incorrect"
-        }
-    ]
+                  // Assert
+                  Expect.isOk result "Value is incorrect"
+                }
+            ]
+
+            testList "GetValue" [
+                test "Should right value be returned" {
+
+                  // Arrange
+                  let value = Guid.NewGuid()
+                  let id = value |> ProductBrandId.create |> fun result -> match result with Ok value -> value | _ -> ProductBrandId.Empty
+
+                  // Act
+                  let result = id |> ProductBrandId.getValue
+
+                  // Assert
+                  Expect.equal value result "Not equals"
+                }
+            ]
+
+            testList "Empty" [
+                test "Should be equals" {
+
+                  // Arrange
+                  let expectedValue = Guid.Empty
+
+                  // Act
+                  let actualValue = ProductBrandId.Empty |> ProductBrandId.getValue
+
+                  // Assert
+                  Expect.equal expectedValue actualValue "Not equals"
+                }
+            ]
+          ]
+
+        testList "ProductBrandName" [
+            testList "Create" [
+                test "Should return error on empty value" {
+
+                  // Arrange
+                  let value = String.Empty
+
+                  // Act
+                  let result = value |> ProductBrandName.create
+
+                  // Assert
+                  Expect.isError result "Value is empty"
+                }
+
+                test "Should return ok on correct value" {
+
+                  // Arrange
+                  let value = "Apple"
+
+                  // Act
+                  let result = value |> ProductBrandName.create
+
+                  // Assert
+                  Expect.isOk result "Value is incorrect"
+                }
+
+            ]
+
+            testList "GetValue" [
+                test "Should right value be returned" {
+
+                  // Arrange
+                  let value = "Apple"
+                  let name = value |> ProductBrandName.create |> fun result -> match result with Ok value -> value | _ -> ProductBrandName.Empty
+
+                  // Act
+                  let result = name |> ProductBrandName.getValue
+
+                  // Assert
+                  Expect.equal value result "Not equals"
+                }
+
+                testList "Empty" [
+                  test "Should be equals" {
+
+                  // Arrange
+                  let expectedValue = String.Empty
+
+                  // Act
+                  let actualValue = ProductBrandName.Empty |> ProductBrandName.getValue
+
+                  // Assert
+                  Expect.equal expectedValue actualValue "Not equals"
+                }
+            ]
+              ]
+          ]
   ]
 
